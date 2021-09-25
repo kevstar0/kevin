9a9717171f9e2778bcfdc34ccaf74843b56a716b
kevstar0 <kevinmanda92@gmail.com>
2021-09-25 19:14:32 +0200

    fixed home on register

--- src/index.js
+++ src/index.js
@@ -50,3 +50,14 @@ if (user !== null) {
     console.log("  Photo URL: " + profile.photoURL);
   });
 }
+
+const functions = require("firebase-functions");
+
+exports.makeUppercase = functions.firestore
+  .document("/messages/{documentId}")
+  .onCreate((snap, context) => {
+    const original = snap.data().original;
+    console.log("Uppercasing", context.params.documentId, original);
+    const uppercase = original.toUpperCase();
+    return snap.ref.set({ uppercase }, { merge: true });
+  });
